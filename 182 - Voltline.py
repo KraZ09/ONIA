66649f21793ea0ab11fcea610cf3c23387366e71
KraZ09 <126799722+KraZ09@users.noreply.github.com>
2026-02-27 16:12:03 +0200

    Remove unused import for train_test_split

--- 182 - Voltline.py
+++ 182 - Voltline.py
@@ -2,7 +2,6 @@
 from sklearn.compose import ColumnTransformer
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import GridSearchCV
